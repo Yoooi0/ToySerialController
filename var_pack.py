b00ef794b6ede436fe252c09d06de06001cd00e1
Yoooi <kamos21@protonmail.com>
2020-04-17 18:16:44 +0200

    Fix var_pack script

--- var_pack.py
+++ var_pack.py
@@ -5,7 +5,7 @@ path = 'Custom\\Scripts\\Yoooi\\ToySerialController\\'
 with open('ADD_ME.cslist', 'r', encoding = 'utf-8') as cslist:
     with ZipFile('Yoooi.ToySerialController.1.var', 'w') as output:
         output.write('meta.json')
-        output.write('LICENSE.md')
+        output.write('LICENSE')
         os.chdir('../../../../')
         output.write(os.path.join(path, 'ADD_ME.cslist'))
         for line in cslist:
