ef80ca2d8805235c357f2a8264ac613f29203812
Yoooi <kamos21@protonmail.com>
2020-06-26 00:02:02 +0200

    Only add .cs files in src and lib folders

--- update_cslist.py
+++ update_cslist.py
@@ -3,5 +3,6 @@ from pathlib import Path
 
 with open('ADD_ME.cslist', 'w', encoding = 'utf-8') as cslist:
     for file in sorted(list(Path('.').rglob('*.cs'))):
-    	cslist.write(str(file) + "\n")
-    	print(file.name)
+    	if str(file).startswith('src') or str(file).startswith('lib'):
+	    	cslist.write(str(file) + "\n")
+    		print(str(file))
