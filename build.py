44a439ce31f1deb189da52d7dab853009221ba4f
Yoooi <kamos21@protonmail.com>
2021-04-01 18:24:50 +0200

    Update build script

---
+++ build.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import glob
+from zipfile import ZipFile
+
+if len(sys.argv) == 2:
+    version = int(sys.argv[1])
+else:
+    version = int(input('var version: '))
+
+cslistName = 'ADD_ME.cslist'
+varName = 'Yoooi.ToySerialController.{}.var'.format(version)
+zipPath = 'Custom\\Scripts\\Yoooi\\ToySerialController\\'
+
+print('Creating "{}"'.format(cslistName))
+with open('ADD_ME.cslist', 'w+', encoding='utf-8') as cslist:
+    for file in sorted(glob.glob('**/*.cs', recursive=True)):
+        if not file.startswith('src') and not file.startswith('lib'):
+            continue
+
+        cslist.write('{}\n'.format(file))
+        print('Adding "{}"'.format(file))
+
+print('Creating "{}"'.format(varName))
+with open(cslistName, 'r', encoding = 'utf-8') as cslist:
+    with ZipFile(varName, 'w') as var:
+        var.write('meta.json')
+        var.write('LICENSE')
+        var.write(cslistName, os.path.join(zipPath, cslistName))
+        for file in [x.strip() for x in cslist]:
+            var.write(file, os.path.join(zipPath, file))
+
+        for file in var.namelist():
+            print('Adding "{}"'.format(file))
--- update_cslist.py
+++
@@ -1,8 +0,0 @@
-import os
-from pathlib import Path
-
-with open('ADD_ME.cslist', 'w', encoding = 'utf-8') as cslist:
-    for file in sorted(list(Path('.').rglob('*.cs'))):
-    	if str(file).startswith('src') or str(file).startswith('lib'):
-	    	cslist.write(str(file) + "\n")
-    		print(str(file))
