851381c1f28291058cca8008f9e411c7236ff3b0
Yoooi <kamos21@protonmail.com>
2022-05-22 15:41:12 +0200

    Make storables usable as trigger actions

--- src/UI/UIManager.cs
+++ src/UI/UIManager.cs
@@ -4,41 +4,86 @@ namespace ToySerialController.UI
 {
     public class UIManager
     {
-        private static UIManager _instance;
-        private MVRScript _plugin;
+        private static UIManager Instance;
+        private MVRScript plugin;
 
-        public static Transform ConfigurableTextFieldPrefab => _instance?._plugin.manager.configurableTextFieldPrefab;
-        public static Transform ConfigurablePopupPrefab => _instance?._plugin.manager.configurablePopupPrefab;
-        public static Transform ConfigurableTogglePrefab => _instance?._plugin.manager.configurableTogglePrefab;
-        public static Transform ConfigurableSliderPrefab => _instance?._plugin.manager.configurableSliderPrefab;
-        public static Transform ConfigurableButtonPrefab => _instance?._plugin.manager.configurableButtonPrefab;
+        public static Transform ConfigurableTextFieldPrefab => Instance.plugin.manager.configurableTextFieldPrefab;
+        public static Transform ConfigurablePopupPrefab => Instance.plugin.manager.configurablePopupPrefab;
+        public static Transform ConfigurableTogglePrefab => Instance.plugin.manager.configurableTogglePrefab;
+        public static Transform ConfigurableSliderPrefab => Instance.plugin.manager.configurableSliderPrefab;
+        public static Transform ConfigurableButtonPrefab => Instance.plugin.manager.configurableButtonPrefab;
 
         private UIManager(MVRScript plugin)
         {
-            _plugin = plugin;
+            this.plugin = plugin;
         }
 
         public static void Initialize(MVRScript plugin)
         {
-            if (_instance != null)
+            if (Instance != null)
                 return;
             
-            _instance = new UIManager(plugin);
+            Instance = new UIManager(plugin);
         }
 
-        public static void RemoveSpacer(UIDynamic o) => _instance._plugin.RemoveSpacer(o);
-        public static void RemoveTextField(JSONStorableString o) => _instance._plugin.RemoveTextField(o);
-        public static void RemoveButton(UIDynamicButton o) => _instance._plugin.RemoveButton(o);
-        public static void RemoveToggle(JSONStorableBool o) => _instance._plugin.RemoveToggle(o);
-        public static void RemoveSlider(JSONStorableFloat o) => _instance._plugin.RemoveSlider(o);
-        public static void RemovePopup(JSONStorableStringChooser o) => _instance._plugin.RemovePopup(o);
+        public static void RemoveSpacer(UIDynamic o) => Instance.plugin.RemoveSpacer(o);
+        public static void RemoveButton(UIDynamicButton o) => Instance.plugin.RemoveButton(o);
 
-        public static UIDynamic CreateSpacer(bool rightSide) => _instance._plugin.CreateSpacer(rightSide);
-        public static UIDynamicToggle CreateToggle(JSONStorableBool storable, bool rightSide) => _instance._plugin.CreateToggle(storable, rightSide);
-        public static UIDynamicTextField CreateTextField(JSONStorableString storable, bool rightSide) => _instance._plugin.CreateTextField(storable, rightSide);
-        public static UIDynamicSlider CreateSlider(JSONStorableFloat storable, bool rightSide) => _instance._plugin.CreateSlider(storable, rightSide);
-        public static UIDynamicButton CreateButton(string label, bool rightSide) => _instance._plugin.CreateButton(label, rightSide);
-        public static UIDynamicPopup CreateScrollablePopup(JSONStorableStringChooser storable, bool rightSide) => _instance._plugin.CreateScrollablePopup(storable, rightSide);
-        public static UIDynamicPopup CreatePopup(JSONStorableStringChooser storable, bool rightSide) => _instance._plugin.CreatePopup(storable, rightSide);
+        public static void RemoveToggle(JSONStorableBool storable)
+        {
+            Instance.plugin.DeregisterBool(storable);
+            Instance.plugin.RemoveToggle(storable);
+        }
+
+        public static void RemoveTextField(JSONStorableString storable)
+        {
+            Instance.plugin.DeregisterString(storable);
+            Instance.plugin.RemoveTextField(storable);
+        }
+
+        public static void RemoveSlider(JSONStorableFloat storable)
+        {
+            Instance.plugin.DeregisterFloat(storable);
+            Instance.plugin.RemoveSlider(storable);
+        }
+
+        public static void RemovePopup(JSONStorableStringChooser storable)
+        {
+            Instance.plugin.DeregisterStringChooser(storable);
+            Instance.plugin.RemovePopup(storable);
+        }
+
+        public static UIDynamic CreateSpacer(bool rightSide) => Instance.plugin.CreateSpacer(rightSide);
+        public static UIDynamicButton CreateButton(string label, bool rightSide) => Instance.plugin.CreateButton(label, rightSide);
+
+        public static UIDynamicToggle CreateToggle(JSONStorableBool storable, bool rightSide)
+        {
+            Instance.plugin.RegisterBool(storable);
+            return Instance.plugin.CreateToggle(storable, rightSide);
+        }
+
+        public static UIDynamicTextField CreateTextField(JSONStorableString storable, bool rightSide)
+        {
+            Instance.plugin.RegisterString(storable);
+            return Instance.plugin.CreateTextField(storable, rightSide);
+        }
+
+        public static UIDynamicSlider CreateSlider(JSONStorableFloat storable, bool rightSide)
+        {
+            Instance.plugin.RegisterFloat(storable);
+            return Instance.plugin.CreateSlider(storable, rightSide);
+        }
+
+        public static UIDynamicPopup CreateScrollablePopup(JSONStorableStringChooser storable, bool rightSide)
+        {
+            Instance.plugin.RegisterStringChooser(storable);
+            return Instance.plugin.CreateScrollablePopup(storable, rightSide);
+        }
+
+        public static UIDynamicPopup CreatePopup(JSONStorableStringChooser storable, bool rightSide)
+        {
+            Instance.plugin.RegisterStringChooser(storable);
+            return Instance.plugin.CreatePopup(storable, rightSide);
+        }
     }
 }
