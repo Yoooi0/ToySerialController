beb6c5d132b6838d481e95e753cec7d3ebe53cb9
axw <11938476+axw-pub@users.noreply.github.com>
2023-08-05 12:04:13 +0200

    Fixed NullRef when looking for females

--- src/MotionSource/AbstractFemaleMotionSource.cs
+++ src/MotionSource/AbstractFemaleMotionSource.cs
@@ -341,7 +341,10 @@ namespace ToySerialController.MotionSource
         {
             var people = Controller.GetAtoms().Where(a => a.type == "Person");
             var femaleUids = people
-                .Where(a => a.GetComponentInChildren<DAZCharacterSelector>().gender == DAZCharacterSelector.Gender.Female)
+                .Where(a => {
+                    var character = a.GetComponentInChildren<DAZCharacterSelector>();
+                    return character != null && character.gender == DAZCharacterSelector.Gender.Female;
+                })
                 .Select(a => a.uid)
                 .ToList();
 
